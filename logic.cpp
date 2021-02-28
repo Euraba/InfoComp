105f79ec0ea9de2212040dc1efa869822948bf38
Euraba <43251950+Euraba@users.noreply.github.com>
2021-02-28 19:20:32 +0200

    Update logic.cpp

--- logic.cpp
+++ logic.cpp
@@ -11,6 +11,8 @@
 std::fstream in ("logic.in", std::ios::in | std::ios::binary) ;
 std::fstream out ("logic.out", std::ios::out) ;
 
+
+// more about this class here https://github.com/Euraba
 class BooleanExpresion {
 private :
     static const int SIGMA = 26 ;
